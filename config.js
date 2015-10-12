6be8af23e3798447818e8241dc8549c1d7380841
Prince Kumar <prince18pat@gmail.com>
2015-10-12 12:46:17 +0900

    Server Config

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
   server: {
     listenPort: 3000,
     securePort: 8433,
-    distFolder: path.resolve(__dirname, '../client/dist'),
+    distFolder: path.resolve(__dirname, '../angularjs-scrum/dist'),
     staticUrl: '/static',
     cookieSecret: 'angular-app'
   }
